42431037343b7cfc535581e401b3cf2f45d38c6e
mabiiak <mabiane.polniak@gmail.com>
2022-10-04 19:33:11 -0300

    feat: implements components created on the home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Title from '../../components/title'
+import Filter from '../../components/filter'
 
 export default function Home() {
   return(
     <div>
+      <Title />
+      <Filter />
     </div>
   )
 }
