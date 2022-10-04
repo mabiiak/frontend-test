a99127a298f284ba1123586e2f82a5549cd194c6
mabiiak <mabiane.polniak@gmail.com>
2022-10-04 19:49:56 -0300

    feat: implement function to capture search element

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../context/provider'
 import Button from '../button'
+import FilterStyle from './style.js'
 
 export default function Filter() {
+  const { search, setSearch } = useContext(Context)
+
+  const captureText = ({ target }) => {
+    const { value } = target
+    setSearch(value)
+  }
+
   return(
-    <div>
-      <input type="text" />
-      <p>Filter by</p>
+    <FilterStyle>
+      <input
+        type="text"
+        placeholder="Insert"
+        onChange={ captureText }
+      />
+      <p>Filter by:</p>
       <Button nameBtn="Movies" />
       <Button nameBtn="Actors" />
       <Button nameBtn="Local" />
-    </div>
+    </FilterStyle>
   )
 }
