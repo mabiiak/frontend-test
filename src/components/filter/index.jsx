a82a27cce6246b15c1813f77ca7fb081f6604b38
mabiiak <mabiane.polniak@gmail.com>
2022-10-04 19:30:25 -0300

    feat: implement base component filter

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Button from '../button'
 
 export default function Filter() {
   return(
-    <>
-    </>
+    <div>
+      <input type="text" />
+      <p>Filter by</p>
+      <Button nameBtn="Movies" />
+      <Button nameBtn="Actors" />
+      <Button nameBtn="Local" />
+    </div>
   )
 }
