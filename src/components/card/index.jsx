2d55ce07f607e6f4eed1c83524477e7d52f21669
mabiiak <mabiane.polniak@gmail.com>
2022-10-05 11:12:05 -0300

    feat: implement component card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card({ image, title, description, director, year }) {
   return(
-    <>
-    </>
+    <div>
+      <img src={ image } alt={`banner from movie ${ title }`} />
+      <p>{ year }</p>
+      <h3>{ title }</h3>
+      <p>{ description }</p>
+    </div>
   )
 }
