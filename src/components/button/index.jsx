9953353fd0d1cbb409c50ce7ba391de871445070
mabiiak <mabiane.polniak@gmail.com>
2022-10-05 14:08:13 -0300

    feat: implements button to restore filter

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../context/provider'
+import fetchMovies from '../../utils/fetchMovies';
 
 export default function Button({ nameBtn }) {
+  const { setSearch, setData } = useContext(Context)
+
+  const resetData = async () => {
+    const response = await fetchMovies()
+    setData(response)
+  }
+
+  const clickButton = () => {
+    setSearch('')
+    resetData()
+  }
+
   return(
-    <button type="button">
+    <button type="button" onClick={ clickButton }>
       { nameBtn }
     </button>
   )
