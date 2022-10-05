f2eb2e61a398408972e7364654481075f519a066
mabiiak <mabiane.polniak@gmail.com>
2022-10-05 14:08:30 -0300

    style: change justify content

--- src/components/renderCard/style.js
+++ src/components/renderCard/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const DisplayCards = styled.section`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: left;
   margin: 0 15vw;
   max-width: 100vw;
 `
