55267bda2483a55c36265e26a36f3146ad437fe5
mabiiak <mabiane.polniak@gmail.com>
2022-10-05 09:52:41 -0300

    feat: create function fecthMovies

---
+++ src/utils/fetchMovies.js
@@ -0,0 +1,13 @@
+const axios = require("axios")
+
+const fetchMovies = async () => {
+  const URL = 'https://ghibliapi.herokuapp.com/films'
+
+  const response = await axios.get(URL)
+    .then((res) => res.data)
+    .catch((error) => console.log(error))
+  
+  return response
+}
+
+export default fetchMovies
