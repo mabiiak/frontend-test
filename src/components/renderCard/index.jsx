5e2a76df81baab9a6b2c0bc19b5ceb8b67a52306
mabiiak <mabiane.polniak@gmail.com>
2022-10-05 14:07:33 -0300

    feat: implement filter by movies

--- src/components/renderCard/index.jsx
+++ src/components/renderCard/index.jsx
@@ -5,27 +5,39 @@ import Card from '../card';
 import DisplayCards from './style';
 
 export default function RenderCard() {
-  const { data, setData } = useContext(Context)
+  const { search, typeSearch, data, setData } = useContext(Context);
 
   const itensRender = async () => {
     const response = await fetchMovies()
     setData(response)
   }
 
-  useEffect(() => { itensRender() }, [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    if (data.length === 0) itensRender()
+
+    const lengthMinSearch = search.length > 0;
+    const lengthMinData = data.length !== 0
+
+    if (lengthMinData && lengthMinSearch && typeSearch === "Movies"){
+      setData(data.filter((movie) => movie.title.toLowerCase().includes(search)))
+    }
+  }, [search, typeSearch])
 
   return(
     <DisplayCards>
       {
-        data.map((movie) => (
-          <Card
-            image={ movie.image }
-            title={ movie.title }
-            description={ movie.description }
-            director={ movie.director }
-            year={ movie.release_date }
-          />
-        ))
+        data.length > 0 &&
+          data.map((movie) => (
+            <Card
+              key={ movie.id }
+              image={ movie.image }
+              title={ movie.title }
+              description={ movie.description }
+              director={ movie.director }
+              year={ movie.release_date }
+            />
+          ))
       }
     </DisplayCards>
   )
