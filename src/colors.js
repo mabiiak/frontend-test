4ac6250bb8816bfbf4c3d6ef8334a1509b19a771
mabiiak <mabiane.polniak@gmail.com>
2022-10-08 17:46:30 -0300

    refactor/styles: change color palette

--- src/colors.js
+++ src/colors.js
@@ -1,4 +1,4 @@
-export const bgColor = '#109CEB'
-export const btnColor = '#F3C15B'
-export const bgCardColor = '#C2EAFC'
-export const textColor = '#464C52'
+export const bgPrincipal = '#2189D2'
+export const bgCards = '#c2e9f7';
+export const bgButton = '#f8b41e';
+export const colorTitle = '#0b2a46';
